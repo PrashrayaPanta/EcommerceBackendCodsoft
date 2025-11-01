4447e6e7f0a52ad9081eb2ae0b6dfd35633fd8b3
Prashraya Panta <prashrayapanta33@gmail.com>
2025-11-01 21:20:18 +0545

    Improve code base:stack trace of error in developemnt enironment only

--- middleware/errHandler.js
+++ middleware/errHandler.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.json({
     message: err.message,
-    stack: err.stack,
+    stack: process.env.NODE_ENV === "development" && err.stack,
   });
 };
 
